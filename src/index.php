44a48c63d5ed4ae235a177915c43e9e39570e68a
Reyshellt <reywilterrero@gmail.com>
2024-06-21 06:43:48 -0400

    Subiendo los archivos

---
+++ src/index.php
@@ -0,0 +1,17 @@
+<?php
+$servername = getenv('MYSQL_HOST');
+$username = getenv('MYSQL_USER');
+$password = getenv('MYSQL_PASSWORD');
+$dbname = getenv('MYSQL_DATABASE');
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+echo "Conexión exitosa a la base de datos";
+echo "\n";
+echo "<h2>Reyshell Terrero 2021-1057<h2>";
+?>
